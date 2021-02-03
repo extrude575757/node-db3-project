a0b531836c4ab613e9aa56619510896bffc23bb3
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-02-02 16:29:25 -0800

    Insits there is no data to print for the records in sqlitestudio

--- queries.sql
+++ queries.sql
@@ -1,5 +1,20 @@
 -- Multi-Table Query Practice
+select 
+p.*, 
+c.CategoryName, c.Description as [CategoryDescription],
+s.CompanyName as [SupplierName], s.Region as [SupplierRegion]
+from [Product] p
+join [Category] c on p.CategoryId = c.id
+join [Supplier] s on s.id = p.SupplierId
 
+
+select 
+*,p,
+c.CategoryName, c.Description as [Category]
+from [Product] p
+
+join [Customer] d on p.ProductName = d.id
+join [Category] c
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
@@ -7,3 +22,6 @@
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+
+
